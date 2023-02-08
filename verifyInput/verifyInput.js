e02357cb953d9b878168fef9e7e8c30b1e062d77
Abhinaya Balaji <abijun4@gmail.com>
2023-02-08 08:40:02 -0800

    verifyInput added

---
+++ verifyInput/verifyInput.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+function makeInputVerifier(minimum, maximum) {
+  return function verify(inputValue) {
+    if(inputValue < minimum) {
+      return 'Input is less than minimum value';
+    } else if(inputValue >= minimum && inputValue <= maximum){
+      return 'Input is in range';
+    }else if(inputValue > maximum){
+    return 'Input is more than maximum value'
+    }
+ }
+}
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const min = parseInt(readLine().trim());
+  const max = parseInt(readLine().trim());
+  const verify = makeInputVerifier(min, max);
+    
+  const input = parseInt(readLine().trim());
+  const result = verify(input);
+
+  ws.write(`${result}\n`);
+  ws.end();
+}
